1223949a5d998fe225625049db660c625d06fc25
Juan Ortiz <ortiz.juan14@gmail.com>
2017-12-17 02:06:04 -0400

    Action Bar

---
+++ Assets/Scripts/Game Play/ActionBar.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ActionBar : MonoBehaviour {
+
+    protected float value;
+    public float speed = 1;
+
+    public virtual void Update()
+    {
+        float progress = Mathf.Cos(speed * Time.time);
+        value = Mathf.Clamp(-progress, -1, 1);
+    }
+
+}
---
+++ Assets/Scripts/UI/Game Play/ActionBarUI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.UI;
+
+public class ActionBarUI : ActionBar {
+
+    private Slider bar;
+
+    private void Start()
+    {
+        bar = GetComponent<Slider>();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+        bar.value = value;
+    }
+}
