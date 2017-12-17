07a9f34050b764adcf15453c2d2cc8ebc2491767
Juan Ortiz <ortiz.juan14@gmail.com>
2017-12-17 15:56:07 -0400

    Demo 3

--- Assets/Scripts/UI/Game Play/Messages In Game/TriggerEvent.cs
+++ Assets/Scripts/UI/Game Play/Messages In Game/TriggerEvent.cs
@@ -6,13 +6,17 @@ public class TriggerEvent : MonoBehaviour {
 
     public enum MessageType {
         HINT,
-        DIALOG
+        DIALOG,
+        ACTION_BAR
     }
+
+    public bool visited;
 
     public MessageType messageType;
 
     private Message hint;
     private Message dialog;
+    private ActionBarUI actionBar;
 
     public MessagesAsset info;
 
@@ -20,11 +24,14 @@ public class TriggerEvent : MonoBehaviour {
     {
         hint = GameObject.Find("HintText").GetComponent<Message>();
         dialog = GameObject.Find("DialogText").GetComponent<Message>();
+        actionBar = GameObject.Find("Action Bar").GetComponent<ActionBarUI>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player")) {
+        if (other.CompareTag("Player") && !visited) {
+
+            visited = true;
 
             if (messageType == MessageType.HINT)
             {
@@ -34,6 +41,11 @@ public class TriggerEvent : MonoBehaviour {
             else if (messageType == MessageType.DIALOG) {
                 dialog.SetMessages(info.messages);
 
+            }
+
+            else if (messageType == MessageType.ACTION_BAR)
+            {
+                actionBar.SetTurn();
             }
         }
     }
